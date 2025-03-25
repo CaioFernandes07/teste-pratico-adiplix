620ac457599ba0e0edac63787b9a1a0601641170
CaioFernandes <caioaires1@gmail.com>
2025-03-25 11:03:29 -0300

    update entrypoint

--- docker/php-fpm/docker-entrypoint.sh
+++ docker/php-fpm/docker-entrypoint.sh
@@ -27,9 +27,5 @@ php artisan key:generate
 # Executando migrations e seeders
 php artisan migrate --seed
 
-# Adicionar permissões de escrita para o diretório de storage
-sudo chmod -R 775 storage
-sudo chmod -R 775 bootstrap/cache
-
 # Executar o comando fornecido (normalmente php-fpm)
 exec "$@"
